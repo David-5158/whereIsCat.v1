694987b4d2b4d43af986d3b9fbb9807ab245dfc7
chawonlee <ss01066865089@gmail.com>
2021-06-05 17:36:07 +0900

    //게시판 리스트 출력XXX(adapter추가)

--- app/src/main/java/com/example/whereiscat/boardActivity.java
+++ app/src/main/java/com/example/whereiscat/boardActivity.java
@@ -1,33 +1,15 @@
 package com.example.whereiscat;
 
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Gallery;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -35,23 +17,26 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import static android.R.layout.activity_list_item;
 
 public class boardActivity extends AppCompatActivity {
 
-    private EditText title, content;
-    Button btnPost;
+    private Button sendbt;
+    private EditText editdt;
+    public String msg;
 
-    ListView board_list;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseRef;
+    private ChildEventListener mChildEventListener;
 
-    private FirebaseAuth mFirebaseAuth;  //파이어베이스 인증
-    private DatabaseReference mDatabaseRef;  //실시간 데이터 베이스
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    List<Object> Array = new ArrayList<Object>();
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,39 +44,85 @@ public class boardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_board);
 
 
-        title = findViewById(R.id.write_title);
-        content = findViewById(R.id.write_content);
-        btnPost = findViewById(R.id.btn_update);
+        sendbt = (Button) findViewById(R.id.button2);
+        editdt = (EditText) findViewById(R.id.editText1);
+        listView = (ListView) findViewById(R.id.board_list);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference("FirebaseLogin");
+        initDatabase();
 
-        btnPost.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Board board = new Board();
-                String UserTitle = title.getText().toString();
-                String UserContent = content.getText().toString();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, new ArrayList<String>());
+        listView.setAdapter(adapter);
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("message"); // 변경값을 확인할 child 이름
 
-                FirebaseDatabase.getInstance().getReference("Board")
-                        .setValue(board).addOnCompleteListener(new OnCompleteListener<Void>(){
-                            @Override
-                            public void onComplete(Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
-
-                                    Board board = new Board();
-                                    board.setTitle(UserTitle);
-                                    board.setContent(UserContent);
-
-                                    mDatabaseRef.child("User Board").child(firebaseUser.getUid()).setValue(board);
-                                }
-                            }
-
-                });
-            }
+        sendbt.setOnClickListener((v) -> {
+          msg = editdt.getText().toString();
+          mDatabaseRef.child("message").push().setValue(msg);
         });
 
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                adapter.clear();
+
+                for (DataSnapshot messageData : dataSnapshot.getChildren()) {
+
+                    String msg2 = messageData.getValue().toString();
+                    Array.add(msg2);
+                    adapter.add(msg2);
+
+                }
+                adapter.notifyDataSetChanged();
+                listView.setSelection(adapter.getCount() - 1);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+    private void initDatabase() {
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        mDatabaseRef = mFirebaseDatabase.getReference("log");
+        mDatabaseRef.child("log").setValue("check");
+
+        mChildEventListener = new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabaseRef.addChildEventListener(mChildEventListener);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDatabaseRef.removeEventListener(mChildEventListener);
+    }
 }
+
+
